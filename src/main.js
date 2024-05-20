c90562ad03a54412a89ebbbaeedd5b14ceca3dde
BVesenjak <benjamin.vesenjak98@gmail.com>
2024-05-20 11:18:39 +0200

    add font-awesome library

--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Import Font Awesome libraries
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Add the icons to the library
+library.add(faInstagram, faTwitter, faFacebook, faLinkedin)
+
+const app = createApp(App)
+
+// Register the FontAwesomeIcon component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Use the router
+app.use(router)
+
+// Mount the app
+app.mount('#app')
